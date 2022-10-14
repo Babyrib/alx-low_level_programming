5f3a9e1b48519a0eb6ebf488a3e34f1a3fc72039
Emmanuel O-O Adjei <eadjei532@gmail.com>
2022-10-13 20:44:06 -0700

    e

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,9 @@
  *  *main-program entry point.
  *   *
  *    *Return: 1 on success.
- */
+ i*/
 int main(void)
 {
-		write(2, "and that piece of art is useful\"
-			       	- Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art is useful\"\n- Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
